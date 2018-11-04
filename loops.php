152bedb1a60376f806248e8a5cb918d91e467c74
alexander iwsifidis <alexis_iwsifidis@hotmail.com>
2018-11-04 23:34:29 +0200

    loops

---
+++ loops.php
@@ -0,0 +1,34 @@
+<?php
+
+$x;
+$y;
+$z;
+
+
+
+for($x=0;$x<=10;$x++)
+{
+
+
+
+
+
+    for($y=0;$y<=10;$y++)
+
+
+
+
+
+        echo $x."*".$y."=".$z=$x*$y."<br>";  
+//    if($z==20){
+//        echo "you will find it";
+//        break;
+//    }
+    echo "<br>";
+  
+
+   
+}
+
+?>    
+
